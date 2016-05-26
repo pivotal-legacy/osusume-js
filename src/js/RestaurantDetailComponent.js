268d6c19ffb1a52140d36b48b41ee243dbb894c1
Chiharu Miya <cmiya@pivotal.io>
2016-05-26 12:07:35 +0900

    Adds details to restaurant details screen.
    [finishes #120252909]
    
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

--- src/js/RestaurantDetailComponent.js
+++ src/js/RestaurantDetailComponent.js
@@ -7,9 +7,47 @@ export default class RestaurantDetailComponent extends React.Component {
         }
     }
 
+    formatAuthor() {
+        var date = new Date(this.props.restaurant.created_at)
+        var author = this.props.restaurant.user ? this.props.restaurant.user.name : ""
+        return `${date.toLocaleDateString()} by ${author}`;
+    }
+
+    formatLikes() {
+        var likes = this.props.restaurant.num_likes
+        if (likes ==  1) { return `${likes} like` }
+        return `${likes} likes`
+    }
+
+    restaurantDetails() {
+        if (this.props.restaurant) {
+            return  (
+              <div>
+                <h1>{this.props.restaurant.name}</h1>
+                <div className="cuisine">{
+                    this.props.restaurant.cuisine ? this.props.restaurant.cuisine.name : ""
+                }
+                </div>
+                <div className="address">{this.props.restaurant.address}</div>
+                <div className="notes">{this.props.restaurant.notes}</div>
+                <div className="num-likes">{ this.formatLikes() }</div>
+                <div className="price-range">{
+                  this.props.restaurant.price_range
+                }</div>
+                <div className="date">{
+                    this.formatAuthor()
+                }</div>
+
+              </div>
+            );
+        } else {
+            return null
+        }
+    }
+
     render() {
         return (
-            <div>{this.props.restaurant ? this.props.restaurant.name : ""}</div>
+          this.restaurantDetails()
         );
     }
 }
--- test/RestaurantDetailComponentTest.js
+++ test/RestaurantDetailComponentTest.js
@@ -6,10 +6,40 @@ import RestaurantDetailComponent from '../src/js/RestaurantDetailComponent';
 
 describe('RestaurantDetailComponent', () => {
     it('displays the restaurant passed in props', () => {
-        let restaurant = {id: 0, name: 'Afuri'};
+        let restaurant = {
+            id: 0,
+            name: 'Afuri',
+            cuisine: {name: "Japanese"},
+            price_range: '¥0~999',
+            user: {id: 0, email: "danny", name: "Danny"},
+            address: "Roppongi",
+            notes: "good",
+            num_likes: 5,
+            created_at: "2016-05-26T10:03:17.736Z"
+        };
         const component = shallow(<RestaurantDetailComponent restaurant={restaurant} />);
 
-        expect(component.contains(<div>Afuri</div>)).toBe(true);
+        expect(component.contains(<h1>Afuri</h1>)).toBe(true);
+        expect(component.contains(<div className="cuisine">Japanese</div>)).toBe(true);
+        expect(component.contains(<div className="price-range">¥0~999</div>)).toBe(true);
+        expect(component.contains(<div className="date">5/26/2016 by Danny</div>)).toBe(true);
+        expect(component.contains(<div className="address">Roppongi</div>)).toBe(true);
+        expect(component.contains(<div className="notes">good</div>)).toBe(true);
+        expect(component.contains(<div className="num-likes">5 likes</div>)).toBe(true);
+    });
+
+    it('displays likes pluralized correctly', () => {
+        expect(shallow(<RestaurantDetailComponent restaurant={{num_likes: 0}} />)
+          .contains(<div className="num-likes">0 likes</div>)
+        ).toBe(true);
+
+        expect(shallow(<RestaurantDetailComponent restaurant={{num_likes: 1}} />)
+          .contains(<div className="num-likes">1 like</div>)
+        ).toBe(true);
+
+        expect(shallow(<RestaurantDetailComponent restaurant={{num_likes: 2}} />)
+          .contains(<div className="num-likes">2 likes</div>)
+        ).toBe(true);
     });
 
     it('fetches the restaurants if no restaurant is passed', () => {
