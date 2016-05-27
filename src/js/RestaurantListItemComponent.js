551e26e89f92523f7f810e3564a2c54b0692a043
Whitney Schaefer <wschaefer@pivotal.io>
2016-05-27 09:35:57 +0900

    Show more restaurant info in list

--- src/js/RestaurantListItemComponent.js
+++ src/js/RestaurantListItemComponent.js
@@ -2,7 +2,19 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default function RestaurantListItemComponent(props) {
+    let cuisineName = '';
+    if (props.restaurant.cuisine) {
+        cuisineName = props.restaurant.cuisine.name;
+    }
     return (
-        <div><Link to={`/restaurants/${props.restaurant.id}`}>{props.restaurant.name}</Link></div>
+        <div>
+            <Link to={`/restaurants/${props.restaurant.id}`}>{props.restaurant.name}</Link>
+            <div className='cuisine'>{cuisineName}</div>
+            <div className='price_range'>{props.restaurant.price_range}</div>
+            <div className='address'>{props.restaurant.address}</div>
+            <div className='number_likes'>{props.restaurant.num_likes}</div>
+            <div className='created_by_user_name'>{props.restaurant.created_by_user_name}</div>
+            <div className='created_at'>{props.restaurant.created_at}</div>
+        </div>
     );
 }
--- test/RestaurantListItemComponentTest.js
+++ test/RestaurantListItemComponentTest.js
@@ -7,9 +7,45 @@ import RestaurantListItemComponent from '../src/js/RestaurantListItemComponent';
 
 describe('RestaurantListItemComponent', () => {
     it('displays the restaurant passed in props', () => {
-        let restaurant = {id: 0, name: 'Afuri'};
+        let restaurant = {
+            id: 0,
+            name: 'Afuri',
+            cuisine: {name: 'Ramen'},
+            price_range: '1000 - 2000',
+            address: 'Iidabashi West Exit',
+            num_likes: 3,
+            created_by_user_name: 'Kalle Anka',
+            created_at: '2016-05-25T01:41:17.125Z',
+        };
         const component = shallow(<RestaurantListItemComponent restaurant={restaurant} />);
 
-        expect(component.contains(<Link to={`/restaurants/0`}>Afuri</Link>)).toBe(true);
+        expect(component.find(Link).length).toEqual(1);
+        expect(component.find(Link).prop('to')).toEqual('/restaurants/0');
+        expect(component.find(Link).prop('children')).toEqual("Afuri");
+
+        expect(component.find('.cuisine').length).toEqual(1);
+        expect(component.find('.cuisine').text()).toEqual('Ramen');
+
+        expect(component.find('.price_range').length).toEqual(1);
+        expect(component.find('.price_range').text()).toEqual('1000 - 2000');
+
+        expect(component.find('.address').length).toEqual(1);
+        expect(component.find('.address').text()).toEqual('Iidabashi West Exit');
+
+        expect(component.find('.number_likes').length).toEqual(1);
+        expect(component.find('.number_likes').text()).toEqual(3);
+
+        expect(component.find('.created_by_user_name').length).toEqual(1);
+        expect(component.find('.created_by_user_name').text()).toEqual('Kalle Anka');
+
+        expect(component.find('.created_at').length).toEqual(1);
+        expect(component.find('.created_at').text()).toEqual('2016-05-25T01:41:17.125Z');
+    });
+
+    it('displays no cuisine if cuisine is null', () => {
+        let restaurant = {id: 0, name: 'Afuri', cuisine: null};
+        const component = shallow(<RestaurantListItemComponent restaurant={restaurant} />);
+        expect(component.find('.cuisine').length).toEqual(1);
+        expect(component.find('.cuisine').text()).toEqual('');
     });
 });
