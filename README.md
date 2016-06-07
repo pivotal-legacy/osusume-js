74d19aad34a0f89a7fdc0edb94f7e4a715647244
Whitney Schaefer <wschaefer@pivotal.io>
2016-06-07 16:09:53 +0900

    Add e2e-tests
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

--- README.md
+++ README.md
@@ -4,5 +4,23 @@
 - `npm test`
 - `npm start`
 
+## End-to-end tests
+### Start server:
+- `cd osusume-java-spring`
+- `java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar`
+
+### Start client:
+- `npm start`
+
+### Start selenium
+- `cd e2e-test`
+- `curl -O http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar`
+- `java -jar selenium-server-standalone-2.53.0.jar`
+
+### Run tests
+- `cd e2e-test`
+- `npm install`
+- `npm test`
+
 ## Deploy
 - `npm run deploy`
---
+++ e2e-test/specs/main_spec.js
@@ -0,0 +1,37 @@
+var expect = require('expect');
+
+describe('restaurants', function() {
+    it('shows list of restaurants and adds a restaurant', function () {
+        browser.url('http://localhost:8000');
+
+        expect(browser.getText('h1')).toEqual('Restaurants');
+        expect(browser.getText('.name')).toInclude('Butagumi');
+
+        browser.click('button');
+
+        expect(browser.getText('h1')).toEqual('find a restaurant');
+
+        var input = browser.element('input');
+        input.setValue('afuri');
+        browser.click('button');
+        browser.waitForExist('.restaurantSuggestion')
+        browser.click('.restaurantSuggestion :nth-of-type(1)');
+
+        var cusineSelect = browser.element('select:nth-of-type(1)');
+        cusineSelect.selectByVisibleText('Japanese');
+        var priceRangeSelect = browser.element('select:nth-of-type(2)');
+        priceRangeSelect.selectByVisibleText('¥0~999');
+        browser.click('button');
+
+        expect(browser.getText('h1')).toEqual('Restaurants');
+        browser.waitUntil(function() {
+          return this.getText('.name').then(function(text) {
+            return text[0] === 'ＡＦＵＲＩ'
+          });
+        });
+
+        browser.click('.name')
+
+        expect(browser.getText('h1')).toEqual('ＡＦＵＲＩ');
+    });
+});
