754cdb829ef6a6abb7f2f18f3f05d364014ada8e
Whitney Schaefer <wschaefer@pivotal.io>
2016-06-28 11:47:32 +0900

    Remove null check on cuisineTypes.
    - The reducer defaults to a empty list for cuisine types.
    
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

--- src/js/new_restaurant/CuisineTypeSelectionComponent.js
+++ src/js/new_restaurant/CuisineTypeSelectionComponent.js
@@ -6,34 +6,26 @@ export default class CuisineTypeSelectionComponent extends React.Component {
     this.onChangeSelectValue = this.onChangeSelectValue.bind(this)
   }
 
-  renderCuisineTypes() {
-    if (this.props.cuisineTypes) {
-      return(
-        <select onChange={this.onChangeSelectValue}>
-          <option>Select a cuisine</option>
-          {this.renderOptions()}
-        </select>
-      )
-    } else {
-      return null;
-    }
-  }
-
   onChangeSelectValue(e) {
     let index = e.target.options.selectedIndex
     let value = e.target.options[index].value
     this.props.changeHandler(value)
   }
 
-  renderOptions() {
-    return this.props.cuisineTypes.map((cuisineType) => {
+  render() {
+    let cuisineTypes = this.props.cuisineTypes.map((cuisineType) => {
       return (
-        <option key={cuisineType.get('id')} value={cuisineType.get('id')}>{cuisineType.get('name')}</option>
+        <option key={cuisineType.get('id')} value={cuisineType.get('id')}>
+          {cuisineType.get('name')}
+        </option>
       )
     })
-  }
 
-  render() {
-    return this.renderCuisineTypes();
+    return (
+      <select onChange={this.onChangeSelectValue}>
+        <option>Select a cuisine</option>
+        {cuisineTypes}
+      </select>
+    )
   }
 }
--- test/new_restaurant/RestaurantNewFormComponentTest.js
+++ test/new_restaurant/RestaurantNewFormComponentTest.js
@@ -6,88 +6,76 @@ import RestaurantNewFormComponent from '../../src/js/new_restaurant/RestaurantNe
 import CuisineTypeSelectionComponent from '../../src/js/new_restaurant/CuisineTypeSelectionComponent'
 import PriceRangeSelectionComponent from '../../src/js/new_restaurant/PriceRangeSelectionComponent'
 
-describe('RestaurantNewFormComponent displays correct components', () => {
-  let suggestion = fromJS({name: 'Afuri', address: 'Roppongi'})
-  let cuisineTypes = [
+describe('RestaurantNewFormComponent', () => {
+  const suggestion = fromJS({name: 'Afuri', address: 'Roppongi'})
+  const cuisineTypes = fromJS([
     {id: 0, name: 'Not Specified'},
     {id: 1, name: 'Japanese'}
-  ]
-  let priceRanges = [
+  ])
+  const priceRanges = fromJS([
     {id: 0, range: 'Not Specified'},
     {id: 1, range: '¥0~999'}
-  ]
+  ])
+  const hashHistory = {
+    push: () => {}
+  }
+  const handler = expect.createSpy()
+  const props = {
+    hashHistory: hashHistory,
+    suggestion: suggestion,
+    priceRanges: priceRanges,
+    cuisineTypes: cuisineTypes,
+    addNewRestaurant: handler,
+    fetchCuisineTypes: () => {},
+    fetchPriceRanges: () => {},
+    fileUploder: () => {}
+  }
 
   afterEach(function () {
     expect.restoreSpies()
   })
 
-  it('shows cuisine types dropdown', () => {
-    let component = shallow(<RestaurantNewFormComponent suggestion={suggestion} cuisineTypes={cuisineTypes} />)
-    let instance = component.instance()
+  it('renders correct elements', () => {
+    const component = shallow(<RestaurantNewFormComponent {...props} />)
+    const instance = component.instance()
+
     expect(component.contains(
       <CuisineTypeSelectionComponent
         cuisineTypes={cuisineTypes}
         changeHandler={instance.cuisineHandleChanged}/>)).toBe(true)
-  })
-
-  it('shows price ranges dropdown', () => {
-    let component = shallow(<RestaurantNewFormComponent
-      suggestion={suggestion} priceRanges={priceRanges} />)
-    let instance = component.instance()
     expect(component.contains(
       <PriceRangeSelectionComponent
         priceRanges={priceRanges}
         changeHandler={instance.priceRangeHandleChanged} />)).toBe(true)
-  })
-
-  it('shows input for file upload', () => {
-    let component = shallow(<RestaurantNewFormComponent
-      suggestion={suggestion}
-      priceRanges={priceRanges}
-    />)
-    let instance = component.instance()
-    expect(component.contains(<label>Add Photo</label>)).toBe(true)
     expect(component.contains(<input type="file" onChange={instance.selectPhoto}/>)).toBe(true)
-  })
-
-  it('shows input area for notes', () => {
-    let component = shallow(<RestaurantNewFormComponent suggestion={suggestion} />)
-    let instance = component.instance()
     expect(component.contains(<textarea className="notes" onChange={instance.noteChanged}></textarea>)).toBe(true)
   })
-})
 
-describe('RestaurantNewFormComponent calls correct function', () => {
-  let suggestion = fromJS({name: 'Afuri', address: 'Roppongi'})
-  let hashHistory = {
-    push: () => {}
-  }
+  it('calls fetchCuisinetypes in componentDidMount', () => {
+    const modifiedProps = Object.assign({}, props, {
+      fetchCuisineTypes: expect.createSpy()
+    })
+    mount(<RestaurantNewFormComponent {...modifiedProps} />)
 
-  it('calls fetchCuisinetypes  in componentDidMount', () => {
-    let props = {
-      fetchCuisineTypes: expect.createSpy(),
-      fetchPriceRanges: () => {}
-    }
-    mount(<RestaurantNewFormComponent suggestion={suggestion} {...props} />)
-    expect(props.fetchCuisineTypes.calls.length).toBe(1)
+    expect(modifiedProps.fetchCuisineTypes.calls.length).toBe(1)
   })
 
-  it('calls fetchPriceRanges  in componentDidMount', () => {
-    let props = {
-      fetchCuisineTypes: () => {},
+  it('calls fetchPriceRanges in componentDidMount', () => {
+    const modifiedProps = Object.assign({}, props, {
       fetchPriceRanges: expect.createSpy()
-    }
-    mount(<RestaurantNewFormComponent suggestion={suggestion} {...props} />)
-    expect(props.fetchPriceRanges.calls.length).toBe(1)
+    })
+    mount(<RestaurantNewFormComponent {...modifiedProps} />)
+
+    expect(modifiedProps.fetchPriceRanges.calls.length).toBe(1)
   })
 
   it('calls onSubmit handler with new restaurant data when "save" is clicked', () => {
-    const handler = expect.createSpy()
-    const component = mount(<RestaurantNewFormComponent hashHistory={hashHistory} suggestion={suggestion} addNewRestaurant={handler} fetchCuisineTypes={()=>{}} fetchPriceRanges={()=>{}} fileUploder={()=>{}}/>)
-    let instance = component.instance()
+    const component = mount(<RestaurantNewFormComponent {...props} />)
+    const instance = component.instance()
     component.find('.notes').get(0).value = '美味しいです'
     component.find('.notes').simulate('change')
     component.find('button').simulate('click')
+
     expect(handler).toHaveBeenCalledWith(
       {
         name: 'Afuri',
@@ -104,37 +92,41 @@ describe('RestaurantNewFormComponent calls correct function', () => {
   it('redirects to the list of restaurants when "save" is clicked', () => {
     const handler = expect.createSpy()
     const hashHandler = expect.spyOn(hashHistory, 'push')
-    const component = mount(<RestaurantNewFormComponent hashHistory={hashHistory} suggestion={suggestion} addNewRestaurant={handler} fetchCuisineTypes={()=>{}} fetchPriceRanges={()=>{}} fileUploder={()=>{}}/>)
-    let instance = component.instance()
+    const component = mount(<RestaurantNewFormComponent {...props} />)
+    const instance = component.instance()
     component.find('button').simulate('click')
+
     expect(hashHandler).toHaveBeenCalledWith('/')
   })
 
   it('sets state when cuisineHandleChanged is called with new cuisine type id ', () => {
-    const component = shallow(<RestaurantNewFormComponent suggestion={suggestion}/>)
-    let instance = component.instance()
+    const component = shallow(<RestaurantNewFormComponent {...props} />)
+    const instance = component.instance()
     instance.cuisineHandleChanged(10)
+
     expect(instance.state.selectedCuisine).toBe(10)
   })
 
   it('sets state when priceRangeHandleChanged is called with new cuisine type id ', () => {
-    const component = shallow(<RestaurantNewFormComponent suggestion={suggestion}/>)
-    let instance = component.instance()
+    const component = shallow(<RestaurantNewFormComponent {...props} />)
+    const instance = component.instance()
     instance.priceRangeHandleChanged('¥0~999')
+
     expect(instance.state.selectedPriceRange).toBe('¥0~999')
   })
 
   it('it sets file in state when input is changed ', () => {
-    const component = shallow(<RestaurantNewFormComponent suggestion={suggestion}/>)
-    let file = {name: "myfile.txt"}
-    let e = {
+    const component = shallow(<RestaurantNewFormComponent {...props} />)
+    const file = {name: "myfile.txt"}
+    const e = {
       target: {
         value: "C:\fakepath\myfile.txt",
         files: [file]
       }
     }
     component.find('input').simulate('change', e)
-    let instance = component.instance()
+    const instance = component.instance()
+
     expect(instance.state.selectedPhoto).toBe(file)
   })
 })
