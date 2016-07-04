47679232d58e300c398bc9abc20b95a2c7ceeda0
Chiharu Miya <cmiya@pivotal.io>
2016-07-04 12:17:11 +0900

    Reference jar from the right location
    Signed-off-by: Whitney Schaefer <wschaefer@pivotal.io>

--- ci-run-e2e-test.sh
+++ ci-run-e2e-test.sh
@@ -1,6 +1,6 @@
 ./gradlew build
 make test-loadsampledata
-OSUSUME_DATABASE_URL=jdbc:postgresql://pivotal:@localhost/osusume-test java -jar applications/api/build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
+OSUSUME_DATABASE_URL=jdbc:postgresql://pivotal:@localhost/osusume-test java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
 JAVA_SERVER_PID=$!
 npm start &
 CLIENT_PID=$!
--- run-e2e-test.sh
+++ run-e2e-test.sh
@@ -1,7 +1,7 @@
 cd ../osusume-java-spring
 ./gradlew build
 make test-loadsampledata
-OSUSUME_DATABASE_URL=jdbc:postgresql://pivotal:@localhost/osusume-test java -jar applications/api/build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
+OSUSUME_DATABASE_URL=jdbc:postgresql://pivotal:@localhost/osusume-test java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
 JAVA_SERVER_PID=$!
 cd ../osusume-js
 
