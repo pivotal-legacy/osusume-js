c3f3a61441bcd4c0d20d27d3967ffad7691bbd50
Whitney Schaefer <wschaefer@pivotal.io>
2016-07-12 13:47:50 +0900

    Style list items in restaurant list.
    Signed-off-by: Kyle Gearhart <kgearhart@pivotal.io>

--- src/js/restaurant_list/RestaurantListItemComponent.js
+++ src/js/restaurant_list/RestaurantListItemComponent.js
@@ -5,19 +5,20 @@ import { pluralize } from '../utils/StringHelpers'
 export default function RestaurantListItemComponent(props) {
   let photo
   if ( props.restaurant.get('photo_urls') && props.restaurant.get('photo_urls').size > 0 ) {
-    photo = <img src={props.restaurant.get('photo_urls').first().get('url')} width={210}  />
+    photo = <img className='photo' src={props.restaurant.get('photo_urls').first().get('url')} width={210}  />
   }
   let date = new Date(props.restaurant.get('updated_at'))
   return (
-    <div>
-      <Link to={`/restaurants/${props.restaurant.get('id')}`}>
+    <Link className='item-with-photo' to={`/restaurants/${props.restaurant.get('id')}`}>
+      {photo}
+      <span className='item-info'>
         <div className='name'>{props.restaurant.get('name')}</div>
-        {photo}
-        <div className='cuisine'>{props.restaurant.get('cuisine').get('name')}</div>
-        <div className='price-range'>{props.restaurant.get('price_range').get('range')}</div>
-        <div className='number-likes'>{pluralize(props.restaurant.get('num_likes'), 'like')}</div>
-        <div className='updated-at'>{date.toLocaleDateString()}</div>
-      </Link>
-    </div>
+        <div className='cuisine'>{props.restaurant.get('cuisine').get('name')} | {props.restaurant.get('price_range').get('range')}</div>
+        <div>
+          <span>{pluralize(props.restaurant.get('num_likes'), 'like')}</span>
+          <span className='pull-right'>{date.toLocaleDateString()}</span>
+        </div>
+      </span>
+    </Link>
   )
 }
