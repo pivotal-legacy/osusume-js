27c096eb043ea07588bb1c5058eb1d4d95d963e8
Whitney Schaefer <wschaefer@pivotal.io>
2016-08-04 16:33:47 +0900

    Revert "Remove unused ci-run-e2e-test.sh"
    This reverts commit 719e346bacb14d16a08c9a855d2f57912624e185.

--- ci-run-e2e-test.sh
+++ ci-run-e2e-test.sh
@@ -1,37 +1,24 @@
-cd ../osusume-java-spring
 make test-refreshdb
 make test-migrate
 make test-loadsampledata
 ./gradlew build
 OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-test java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
 JAVA_SERVER_PID=$!
-cd ../osusume-js
-
 npm start &
 CLIENT_PID=$!
-
-cd e2e-test
 if [ ! -f selenium-server-standalone-2.53.0.jar ]; then
   curl -O http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar
 fi
 java -jar selenium-server-standalone-2.53.0.jar &
 SELENIUM_PID=$!
-
-curl http://localhost:8080
-while [ $? -ne 0 ]; do
-    echo Waiting for server
-    SLEEP 1
-    curl http://localhost:8080
-done
-
-echo Server ready!
-
+sleep 5
+cd e2e-test
 npm install
 npm test
 TEST_EXIT_STATUS=$?
 
 kill $JAVA_SERVER_PID
-kill $SELENIUM_PID
 kill $CLIENT_PID
+kill $SELENIUM_PID
 
 exit $TEST_EXIT_STATUS
