65033a3926bc938f865309e259c7ddf10024a7bd
Andrew Wright <anwright@pivotal.io>
2016-08-04 17:34:20 +0900

    Wait for each background task to start in e2e-test script
    Signed-off-by: Whitney Schaefer <wschaefer@pivotal.io>

--- run-e2e-test.sh
+++ run-e2e-test.sh
@@ -1,14 +1,41 @@
 cd ../osusume-java-spring
+
+
+echo ==== Setting up DB & Starting Server ====
+
 make test-refreshdb
 make test-migrate
 make test-loadsampledata
 ./gradlew build
 OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-test java -jar build/libs/osusume-java-spring-0.0.1-SNAPSHOT.jar &
 JAVA_SERVER_PID=$!
-cd ../osusume-js
 
+curl http://localhost:8080
+while [ $? -ne 0 ]; do
+    echo Waiting for server
+    SLEEP 1
+    curl http://localhost:8080
+done
+echo Server ready!
+
+
+echo ==== Starting Client ====
+
+cd ../osusume-js
+npm install
 npm start &
 CLIENT_PID=$!
+
+curl http://localhost:8000
+while [ $? -ne 0 ]; do
+    echo Waiting for client
+    SLEEP 1
+    curl http://localhost:8000
+done
+echo Client ready!
+
+
+echo ==== Starting Selenium ====
 
 cd e2e-test
 if [ ! -f selenium-server-standalone-2.53.0.jar ]; then
@@ -17,14 +44,16 @@ fi
 java -jar selenium-server-standalone-2.53.0.jar &
 SELENIUM_PID=$!
 
-curl http://localhost:8080
+curl http://127.0.0.1:4444/wd/hub
 while [ $? -ne 0 ]; do
-    echo Waiting for server
+    echo Waiting for Selenium server
     SLEEP 1
-    curl http://localhost:8080
+    curl http://127.0.0.1:4444/wd/hub
 done
+echo Selenium Server ready!
 
-echo Server ready!
+
+echo ==== Running Tests ====
 
 npm install
 npm test
