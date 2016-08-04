e9da15f4220d249d0e5a7205d7c0fa50d7d6f3eb
Andrew Wright <anwright@pivotal.io>
2016-08-04 14:52:39 +0900

    e2e tests sleep until the server is running
    - updated the run-e2e-test script to work both locally and for CI
    
    Signed-off-by: Whitney Schaefer <wschaefer@pivotal.io>

--- run-e2e-test.sh
+++ run-e2e-test.sh
@@ -1,4 +1,5 @@
 cd ../osusume-java-spring
+make test-refreshdb
 make test-migrate
 make test-loadsampledata
 ./gradlew build
@@ -6,13 +7,24 @@ OSUSUME_DATABASE_URL=jdbc:postgresql://localhost/osusume-test java -jar build/li
 JAVA_SERVER_PID=$!
 cd ../osusume-js
 
+npm start &
+CLIENT_PID=$!
+
 cd e2e-test
 if [ ! -f selenium-server-standalone-2.53.0.jar ]; then
   curl -O http://selenium-release.storage.googleapis.com/2.53/selenium-server-standalone-2.53.0.jar
 fi
 java -jar selenium-server-standalone-2.53.0.jar &
 SELENIUM_PID=$!
-sleep 5
+
+curl http://localhost:8080
+while [ $? -ne 0 ]; do
+    echo Waiting for server
+    SLEEP 1
+    curl http://localhost:8080
+done
+
+echo Server ready!
 
 npm install
 npm test
@@ -20,5 +32,6 @@ TEST_EXIT_STATUS=$?
 
 kill $JAVA_SERVER_PID
 kill $SELENIUM_PID
+kill $CLIENT_PID
 
 exit $TEST_EXIT_STATUS
