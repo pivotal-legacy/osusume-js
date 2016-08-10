ce6c40a756df3816857608517caf028255c724b2
Andrew Wright <anwright@pivotal.io>
2016-08-10 10:12:53 +0900

    Try putting built files in the root directory
    Signed-off-by: Yuki Nishijima <ynishijima@pivotal.io>

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: "./src/js/main.js",
     output: {
-        path: __dirname + '/built-osusume-js',
+        path: __dirname,
         filename: 'index_bundle.js'
     },
     plugins: [
